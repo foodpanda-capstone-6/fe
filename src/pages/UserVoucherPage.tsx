d5432b4243e266660b9cc3483be20acae0e8fdf1
foodpandaKai <kaichung.yeo@foodpanda.com>
2023-02-08 22:51:58 +0800

    voucher tab

--- src/pages/UserVoucherPage.tsx
+++ src/pages/UserVoucherPage.tsx
@@ -8,30 +8,46 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Modal,
   Tab,
   Tabs,
   Typography,
 } from "@mui/material";
-import Header from "../components/Header";
+import { TabPanel, TabContext, TabList } from "@mui/lab";
 import { useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   isLogin: boolean;
 }
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const UserVoucherPage: React.FC<Props> = ({ isLogin }) => {
   const navigate: (path: string) => void = useNavigate();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     if (isLogin === false) {
       navigate("/login");
     }
-  }, []);
+  }, [isLogin]);
 
-  const [value, setValue] = React.useState("one");
+  const [value, setValue] = React.useState("myVoucher");
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+  console.log(value);
   return (
     <>
       <main>
@@ -45,98 +61,68 @@ const UserVoucherPage: React.FC<Props> = ({ isLogin }) => {
               alt="PauPauYourVouchers"
               src="../src/assets/images/YourVouchersImg.png"
             />
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                textColor="secondary"
-                indicatorColor="secondary"
-                aria-label="secondary tabs example"
-                sx={{ color: "#FF2B85" }}
+            <TabContext value={value}>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
               >
-                <Tab value="one" label="Your Vouchers" />
-                <Tab value="two" label="Voucher Store" />
-              </Tabs>
-            </Stack>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://via.placeholder.com/150"
-                  alt="voucher image"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    $10 PauPau Dollars
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Description
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" sx={{ color: "#FF2B85" }}>
-                  Purchase
-                </Button>
-              </CardActions>
-            </Card>
-
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://via.placeholder.com/150"
-                  alt="voucher image"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    $15 PauPau Dollars
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Description
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" sx={{ color: "#FF2B85" }}>
-                  Purchase
-                </Button>
-              </CardActions>
-            </Card>
-
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://via.placeholder.com/150"
-                  alt="voucher image"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    $30 PauPau Dollars
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Description
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" sx={{ color: "#FF2B85" }}>
-                  Purchase
-                </Button>
-              </CardActions>
-            </Card>
+                <TabList
+                  value={value}
+                  onChange={handleChange}
+                  textColor="secondary"
+                  indicatorColor="secondary"
+                  aria-label="secondary tabs example"
+                  sx={{ color: "#FF2B85" }}
+                >
+                  <Tab value="myVoucher" label="Your Vouchers" />
+                  <Tab value="voucherStore" label="Voucher Store" />
+                </TabList>
+              </Stack>
+              <TabPanel value="myVoucher">
+                <Card style={{ marginTop: "6px" }}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        $10 PFD
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Expiry
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      sx={{ color: "#FF2B85" }}
+                      onClick={handleOpen}
+                    >
+                      Send
+                    </Button>
+                  </CardActions>
+                </Card>
+              </TabPanel>
+              <TabPanel value="voucherStore">my store</TabPanel>
+            </TabContext>
           </Container>
         </Box>
       </main>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 };
