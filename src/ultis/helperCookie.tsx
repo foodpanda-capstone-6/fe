9fca6d81235d073a4f40935e6dc690f34fbf4fbc
Noel Lim <noellimx@gmail.com>
2023-02-10 23:41:31 +0800

    fix function should return undefined instead of null

--- src/ultis/helperCookie.tsx
+++ src/ultis/helperCookie.tsx
@@ -4,7 +4,6 @@ export const setCookie = (name: string) => {
   date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
   expires = "; expires=" + date.toUTCString();
   document.cookie = "username" + "=" + (name || "") + expires + "; path=/";
-  return null;
 };
 
 export const getCookie = (name: string) => {
@@ -15,10 +14,8 @@ export const getCookie = (name: string) => {
     while (c.charAt(0) == " ") c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-  return null;
 };
 
 export const eraseCookie = (name: string) => {
   document.cookie = name + "=; Max-Age=-99999999;";
-  return null;
 };
